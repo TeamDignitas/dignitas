b264e66cb4252b26a65c8b3522b5356de3d37bc2
Cătălin Frâncu <cata@francu.com>
2019-01-13 19:39:45 +0200

    Cleanup OS.php.

--- lib/OS.php
+++ lib/OS.php
@@ -8,13 +8,11 @@ class OS {
   }
 
   static function executeAndAssert($command) {
-    $exit_code = 0;
-    $output = null;
     Log::info("Executing $command");
-    exec($command, $output, $exit_code);
-    if ($exit_code) {
+    exec($command, $output, $exitCode);
+    if ($exitCode) {
       Log::error('Output: ' . implode("\n", $output));
-      self::errorAndExit("Failed command: $command (code $exit_code)");
+      self::errorAndExit("Failed command: $command (code $exitCode)");
     }
   }
 
