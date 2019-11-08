452e4c56f00dfa499ac4f60eb7b8cb3cdaa936c5
Cătălin Frâncu <cata@francu.com>
2019-11-08 13:00:23 +0200

    [#32] Better selection of active review queues for user.

--- routes/aggregate/dashboard.php
+++ routes/aggregate/dashboard.php
@@ -3,13 +3,22 @@
 User::enforce(1); /* just ensure user is logged in */
 
 if (User::may(User::PRIV_REVIEW)) {
-  // get reasons of pending reviews
+  // get reasons of reviews that are (a) pending and (b) not signed off by the
+  // current user
   $reasons = Model::factory('Review')
-    ->select('reason')
+    ->table_alias('r')
+    ->select('r.reason')
     ->distinct()
-    ->where('status', Review::STATUS_PENDING)
+    ->raw_join(
+      'left join review_log',
+      'r.id = rl.reviewId and rl.userId = ?',
+      'rl',
+      [User::getActiveId()])
+    ->where('r.status', Review::STATUS_PENDING)
+    ->where_null('rl.id')
     ->order_by_asc('reason')
     ->find_many();
+
   $reasons = Util::objectProperty($reasons, 'reason');
   Smart::assign('activeReviewReasons', $reasons);
 }
