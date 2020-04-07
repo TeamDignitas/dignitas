143c1dc1a36572f5324aa6198ddf51b4de9c6696
Cătălin Frâncu <cata@francu.com>
2020-04-07 15:58:52 +0300

    [#36] Show a flash message when queueing post for reopening.

--- lib/model/Review.php
+++ lib/model/Review.php
@@ -332,6 +332,7 @@ class Review extends Proto {
 
     if ($ts && ((time() - $ts) / 86400 < Config::EDIT_REOPEN_DAYS)) {
       self::ensure($obj, Ct::REASON_REOPEN);
+      FlashMessage::add(_('info-added-reopen-queue'), 'success');
     }
   }
 
