0717e5530faba1dfc5d75e735539f93382574558
Cătălin Frâncu <cata@francu.com>
2020-04-15 09:25:48 +0300

    [#16] Update scripts to use relation and entity types.

--- scripts/computeLoyalties.php
+++ scripts/computeLoyalties.php
@@ -3,7 +3,7 @@
  * This script goes through entities and their relations to compute entity
  * loyalties. An entity's loyalty is defined as a set of pairs (p_1, f_1)
  * ... (p_k, f_k) where
- *   - p_1 ... p_k are known parties
+ *   - p_1 ... p_k are known sinks (i.e. parties)
  *   - 0 ≤ f_i ≤ 1
  *   - sum(f_i) = 1
  *
@@ -11,8 +11,8 @@
  * the entity's relations, we assign weights to each relation (e.g. a "party
  * member" relation will weigh more than a "close relative" relation, which
  * will weigh more than a "distant relative" relation).  Normalize weights so
- * they sum up to 1. Define parties as absorbing states, then compute the
- * absorption probabilities for each party using the power iteration method
+ * they sum up to 1. Define sinks as absorbing states, then compute the
+ * absorption probabilities for each sink using the power iteration method
  * described here:
  *
  * https://cs.stackexchange.com/a/75271/117308
@@ -23,55 +23,36 @@ require_once __DIR__ . '/../lib/Core.php';
 const NUM_ITERATIONS = 100;
 const THRESHOLD = 0.01; // discard fractions below this value and renormalize
 
-// Weights of edges for various relation and entity types. If a case is not
-// described here, then the relation does not yield an edge in the Markov
-// graph. Each element is a (from, to) pair indicating weights for the forward
-// and back edges. For instance, a company and a person should have edges in
-// both directions.
-const TYPE_WEIGHTS = [
-  Entity::TYPE_PERSON => [
-    Relation::TYPE_MEMBER => [
-      Entity::TYPE_PARTY => [ 1.0, 0.0 ], // 0.0 means no edge from party to person
-    ],
-    Relation::TYPE_CLOSE_RELATIVE => [
-      Entity::TYPE_PERSON => [ 0.5, 0.0 ], // 0.0 because there should be a reciprocal relation
-    ],
-    Relation::TYPE_DISTANT_RELATIVE => [
-      Entity::TYPE_PERSON => [ 0.25, 0.0 ],
-    ],
-  ],
-];
-
 LocaleUtil::change('en_US.utf8');
 
-// Build a map such that $partyIds[k] = true iff Entity ID k is a party.
-$parties = Entity::get_all_by_type_status(Entity::TYPE_PARTY, Ct::STATUS_ACTIVE);
-$partyIds = Util::objectProperty($parties, 'id');
+// Build a map such that $sinkIds[k] = true iff Entity ID k is a sink.
+$sinks = Model::factory('Entity')
+  ->table_alias('e')
+  ->select('e.*')
+  ->join('entity_type', [ 'e.entityTypeId', '=', 'et.id' ],  'et')
+  ->where('e.status', Ct::STATUS_ACTIVE)
+  ->where('et.loyaltySink', true)
+  ->find_many();
+$sinkIds = Util::objectProperty($sinks, 'id');
 
-// Load all relations and convert them to (from, to, weight) edges. Map these
-// by from. The result is an array of $from => [ [ $to, $weight ], ... ].
+// Load all relations that have weights and convert them to (from, to, weight) edges.
+// Map these by from. The result is an array of $from => [ [ $to, $weight ], ... ].
 $relations = Model::factory('Relation')
   ->table_alias('r')
   ->select('r.*')
-  ->select('f.type', 'fromType')
-  ->select('t.type', 'toType')
+  ->select('rt.weight')
   ->join('entity', ['r.fromEntityId', '=', 'f.id'], 'f')
   ->join('entity', ['r.toEntityId', '=', 't.id'], 't')
+  ->join('relation_type', [ 'r.relationTypeId', '=', 'rt.id' ],  'rt')
   ->where('f.status', Ct::STATUS_ACTIVE)
   ->where('t.status', Ct::STATUS_ACTIVE)
+  ->where_gt('rt.weight', 0)
   ->find_many();
 
 // first store relations as [ $from => [ $to => $weight ]];
 $forwardMap = [];
-
 foreach ($relations as $r) {
-  $factors = TYPE_WEIGHTS[$r->fromType][$r->type][$r->toType] ?? null;
-  if ($factors) {
-    $weight = $r->getWeight();
-
-    addEdge($forwardMap, $r->fromEntityId, $r->toEntityId, $weight * $factors[0]);
-    addEdge($forwardMap, $r->toEntityId, $r->fromEntityId, $weight * $factors[1]);
-  }
+  addEdge($forwardMap, $r->fromEntityId, $r->toEntityId, $r->getWeight() * $r->weight);
 }
 
 // normalize weights so the sum of outgoing weights is 1
@@ -80,13 +61,19 @@ normalizeMap($forwardMap);
 // flip the map to get the weights map by toEntityId
 $backMap = flip($forwardMap);
 
-// make parties absorbing
-foreach ($partyIds as $id) {
+// make sinks absorbing
+foreach ($sinkIds as $id) {
   $backMap[$id][$id] = 1.0;
 }
 
-$entities = Entity::get_all_by_type_status(Entity::TYPE_PERSON, Ct::STATUS_ACTIVE);
-foreach ($entities as $e) {
+$sources = Model::factory('Entity')
+  ->table_alias('e')
+  ->select('e.*')
+  ->join('entity_type', [ 'e.entityTypeId', '=', 'et.id' ],  'et')
+  ->where('e.status', Ct::STATUS_ACTIVE)
+  ->where('et.loyaltySource', true)
+  ->find_many();
+foreach ($sources as $e) {
   // compute x = x_0 * P^T
   $loyalties = multiply($e, $backMap);
   normalizeLoyalties($loyalties);
@@ -115,7 +102,6 @@ function normalizeMap(&$map) {
 }
 
 function flip (&$map) {
-  // now build the back map
   $result = [];
   foreach ($map as $from => $row) {
     foreach ($row as $to => $weight) {
