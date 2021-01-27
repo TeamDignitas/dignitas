c2db3c6725e5dd582829d897f125891b2f2d1d3b
Cătălin Frâncu <cata@francu.com>
2021-01-27 13:35:37 +0200

    [#195] Side sheet for answer resources.

--- www/js/autoload/statement/view.js
+++ www/js/autoload/statement/view.js
@@ -5,6 +5,30 @@ $(function() {
     if (hash) {
       $(hash).addClass('highlighted');
     }
+
+    $('#answer-resources-minimize').click(minimizeAnswerResources);
+    $('#answer-resources-maximize').click(maximizeAnswerResources);
+    $('#answer-edit').focusin(initAnswerResources);
+  }
+
+  function initAnswerResources() {
+    // only maximize it the first time we focus the form
+    if (!$('#answer-resources').hasClass('minimized')) {
+      maximizeAnswerResources();
+    }
+  }
+
+  function minimizeAnswerResources() {
+    $('#answer-resources')
+      .removeClass('maximized')
+      .addClass('minimized');
+  }
+
+  function maximizeAnswerResources() {
+    console.log('hello');
+    $('#answer-resources')
+      .removeClass('minimized')
+      .addClass('maximized');
   }
 
   init();
