7156d556039e67103718f348835752dd5315cbe1
Cătălin Frâncu <cata@francu.com>
2021-03-12 13:06:24 +0200

    [#239] More work on the ArchiveBox archiver.

--- scripts/scheduleArchive.php
+++ scripts/scheduleArchive.php
@@ -2,8 +2,7 @@
 <?php
 /**
  * This script examines recent changes, extracts archivable links and
- * schedules them for addition to / removal from the archive. The actual
- * interface with the archiving framework happens in scripts/runArchive.php.
+ * adds them to / removes them from the archive.
  *
  * Use with -s <M> or --since <M> to only consider objects modified within the
  * last M minutes. Without this flag, the script examines everything.
@@ -19,9 +18,15 @@ $opts = getopt('ns:', ['dry-run', 'since:']);
 $dryRun = isset($opts['n']) || isset($opts['dry-run']);
 $since = (int)($opts['s'] ?? $opts['since'] ?? 0);
 
+// first, create/update/delete ArchivedLink objects as needed
 foreach (getArchivableClasses() as $class) {
-  updateLinks($class, $since, $dryRun);
+  updateClass($class, $since);
 }
+
+// second, run the archiver
+$archiver = getArchiver($dryRun);
+$addJobs = getAddJobs();
+$archiver->add($addJobs);
 
 Log::info('finished');
 
@@ -55,9 +60,8 @@ function getArchivableClasses() {
  * @param string $class A class name.
  * @param int $since Consider objects modified at most $since minutes ago, or
  *   all objects if $since = 0.
- * @param bool $dryRun Print what would happen without changing anything.
  */
-function updateLinks(string $class, int $since, bool $dryRun) {
+function updateClass(string $class, int $since) {
   // load the objects
   $objects = Model::factory($class);
 
@@ -70,39 +74,46 @@ function updateLinks(string $class, int $since, bool $dryRun) {
 
   // examine the objects
   foreach ($objects as $obj) {
-    Log::info('Analyzing %s#%d (%s)', $class, $obj->id, $obj);
+    updateLinks($obj);
+  }
+}
 
-    // extract URLs and add them to a map indexed by URL
-    $urls = $obj->getArchivableUrls();
-    $newMap = array_fill_keys($urls, true);
+/**
+ * Extracts links from one object containing archivable URLs.
+ */
+function updateLinks($obj) {
+  Log::info('Analyzing %s#%d (%s)', get_class($obj), $obj->id, $obj);
 
-    // load the existing archived links
-    $als = ArchivedLink::getForObject($obj);
-    foreach ($als as $al) {
-      if (isset($newMap[$al->url])) {
-        // URL is archived and we see it again: keep it. Conveniently, if for any
-        // reason we have the URL multiple times, this will only keep one copy.
-        unmarkForDeletion($al, $dryRun);
-        unset($newMap[$al->url]);
-      } else {
-        // URL is archived but no longer needed: delete it.
-        markForDeletion($al, $dryRun);
-      }
+  // extract URLs and add them to a map indexed by URL
+  $urls = $obj->getArchivableUrls();
+  $newMap = array_fill_keys($urls, true);
+
+  // load the existing archived links
+  $als = ArchivedLink::getForObject($obj);
+  foreach ($als as $al) {
+    if (isset($newMap[$al->url])) {
+      // URL is archived and we see it again: keep it. Conveniently, if for any
+      // reason we have the URL multiple times, this will only keep one copy.
+      unmarkForDeletion($al);
+      unset($newMap[$al->url]);
+    } else {
+      // URL is archived but no longer needed: delete it.
+      markForDeletion($al);
     }
+  }
 
-    // now schedule for insertion the URLs remaining in $newMap
-    foreach ($newMap as $url => $ignored) {
-      markForArchival($obj, $url, $dryRun);
-    }
+  // now schedule for insertion the URLs remaining in $newMap
+  foreach ($newMap as $url => $ignored) {
+    markForArchival($obj, $url);
   }
 }
 
 /**
  * Ensures that an ArchivedLink is marked for deletion.
  */
-function markForDeletion(ArchivedLink $al, bool $dryRun) {
+function markForDeletion(ArchivedLink $al) {
   if ($al->status != ArchivedLink::STATUS_DELETED) {
-    if ($dryRun) {
+    if ($GLOBALS['dryRun']) {
       Log::info('  DRY RUN marking for deletion: [%s]', $al->url);
     } else {
       Log::info('  marking for deletion: [%s]', $al->url);
@@ -114,9 +125,9 @@ function markForDeletion(ArchivedLink $al, bool $dryRun) {
 /**
  * Ensures that an ArchivedLink will not be deleted.
  */
-function unmarkForDeletion(ArchivedLink $al, bool $dryRun) {
+function unmarkForDeletion(ArchivedLink $al) {
   if ($al->status == ArchivedLink::STATUS_DELETED) {
-    if ($dryRun) {
+    if ($GLOBALS['dryRun']) {
       Log::info('  DRY RUN unmarking for deletion: [%s]', $al->url);
     } else {
       Log::info('  unmarking for deletion: [%s]', $al->url);
@@ -130,11 +141,42 @@ function unmarkForDeletion(ArchivedLink $al, bool $dryRun) {
  *
  * @param Precursor $obj Object containing the URL.
  */
-function markForArchival(Precursor $obj, string $url, bool $dryRun) {
-  if ($dryRun) {
+function markForArchival(Precursor $obj, string $url) {
+  if ($GLOBALS['dryRun']) {
     Log::info('  DRY RUN marking for archival: [%s]', $url);
   } else {
     Log::info('  marking for archival: [%s]', $url);
     ArchivedLink::create($obj, $url);
   }
+}
+
+/**
+ * @param bool $dryRun Tell this archiver to print what would happen without changing anything.
+ * @return Archiver
+ */
+function getArchiver($dryRun) {
+  $class = Config::ARCHIVER_CLASS;
+  Log::info('creating a new archiver of type %s', $class);
+
+  if (!class_exists($class)) {
+    $msg = "Class {$class} does not exist. Please check Config.php.";
+    Log::error($msg);
+    die("{$msg}\n");
+  }
+
+  $options = Config::ARCHIVER_OPTIONS;
+  $options['dryRun'] = $dryRun;
+  return new $class($options);
+}
+
+/**
+ * Returns a list of ArchivedLinks that have yet to be archived.
+ *
+ * @return array<ArchivedLink>
+ */
+function getAddJobs() {
+  return Model::factory('ArchivedLink')
+    ->where('status', ArchivedLink::STATUS_NEW)
+    ->order_by_asc('createDate')
+    ->find_many();
 }
