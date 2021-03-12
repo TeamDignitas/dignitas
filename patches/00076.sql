3dda402369881da258ae2572a8465a8172580e0e
Cătălin Frâncu <cata@francu.com>
2021-03-12 17:11:55 +0200

    [#239] Store path information about archived URLs

--- patches/00076.sql
+++ patches/00076.sql
@@ -1,6 +1,8 @@
 create table archived_link (
   id int not null auto_increment,
   status int not null default 0,
+  timestamp varchar(50) not null default '',
+  path varchar(1024) not null default '',
 
   objectType int not null default 0,
   objectId int not null default 0,
