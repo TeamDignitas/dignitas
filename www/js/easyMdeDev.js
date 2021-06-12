36ea21dd08b5458daa1a8238831b59b167ccd43c
Cătălin Frâncu <cata@francu.com>
2021-06-12 17:20:48 +0300

    [#275] Fix answer resources side sheet.

--- www/js/easyMdeDev.js
+++ www/js/easyMdeDev.js
@@ -68,7 +68,7 @@ $(function() {
       name: 'answer-resources-link',
       action: null,
       icon: '<i class="material-icons">lightbulb</i>',
-      className: 'answer-resources-link float-right d-none',
+      className: 'answer-resources-link float-end d-none',
       title: _('easymde-resources'),
     },
   ];
