8ef9331001c59276aa9e2c8d2afb2626debf652e
Cătălin Frâncu <cata@francu.com>
2021-09-24 17:18:11 +0300

    [#200] Bootstrap theme for dark mode, v0.1.

--- scripts/recompileCss.sh
+++ scripts/recompileCss.sh
@@ -7,9 +7,9 @@ ROOT_DIR=`dirname $CWD`
 cd $ROOT_DIR
 echo "The root of your client appears to be $ROOT_DIR"
 
-INPUT=www/scss/main.scss
+INPUT=www/scss/main-dark.scss
 OUTPUT=www/css/third-party/bootstrap.min.css
 export SASS_PATH=www/scss/third-party/bootstrap/
 
 echo "Recompiling $INPUT into $OUTPUT"
-sassc -t compressed $INPUT > $OUTPUT
+sassc -t nested $INPUT > $OUTPUT
